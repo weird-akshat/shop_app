e486438f5b2809d78c2df5b0b75b6658ad7b08b9
Akshat Pandey <thatweirdakshat@gmail.com>
2025-02-05 12:04:06 +0530

    adding the search bar and shoes collection text

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    'Shoes \nCollection',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Expanded(
+                    child: TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    hintText: 'Search',
+                    border: OutlineInputBorder(
+                      borderRadius:
+                          BorderRadius.horizontal(left: Radius.circular(40)),
+                    ),
+                  ),
+                ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
